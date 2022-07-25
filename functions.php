bdd2f19d476f4d9a7676e9f6ad548eeadd03f466
Nimbus Ch <mrchatkiat409@gmail.com>
2022-07-25 22:23:37 +0700

    first commit

---
+++ functions.php
@@ -0,0 +1,78 @@
+<?php 
+
+    define('DB_SERVER', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'smoke');
+    
+    class DB_con {
+
+        function __construct() {
+            $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+            $this->dbcon = $conn;
+
+            if (mysqli_connect_errno()) {
+                echo "Failed to connect to MySQL : " . mysqli_connect_error();
+            }
+        }
+
+        public function insert($status, $location) {
+            $result = mysqli_query($this->dbcon, "INSERT INTO tbcamera(status_cm, location_cm) VALUES('$status', '$location')");
+            return $result;
+        }
+
+        public function fetchdata() {
+            $result = mysqli_query($this->dbcon, "SELECT * FROM tbcamera");
+            return $result;
+        }
+
+        public function fetchonerecord($cameraid) {
+            $result = mysqli_query($this->dbcon, "SELECT * FROM tbcamera WHERE id_camera = '$cameraid'");
+            return $result;
+        }
+
+        public function update($status, $location, $cameraid) {
+            $result = mysqli_query($this->dbcon, "UPDATE tbcamera SET 
+                status_cm = '$status',
+                location_cm = '$location',
+                WHERE id_camera = '$cameraid'
+            ");
+            return $result;
+        }
+
+        public function delete($cameraid) {
+            $deleterecord = mysqli_query($this->dbcon, "DELETE FROM tbcamera WHERE id_camera = '$cameraid'");
+            return $deleterecord;
+        }
+
+        public function count(){
+            $result= mysqli_query($this->dbcon,"SELECT count(*) as total from tbcamera where status_cm like 'on'");
+                return $result;
+               
+        }
+
+        public function count_off(){
+            $result= mysqli_query($this->dbcon,"SELECT count(*) as total from tbcamera where status_cm like 'off'");
+                return $result;
+               
+        }
+        
+        public function usernameavailable($uname) {
+            $checkuser = mysqli_query($this->dbcon, "SELECT username FROM tbl_user WHERE username = '$uname'");
+            return $checkuser;
+        }
+
+
+        public function signin($uname, $password) {
+            $signinquery = mysqli_query($this->dbcon, "SELECT id, fullname FROM tbl_user WHERE username = '$uname' AND password = '$password'");
+            return $signinquery;
+        }
+        public function registration($fname, $uname, $password, $uemail) {
+            $reg = mysqli_query($this->dbcon, "INSERT INTO tbl_user(fullname, username, password, email) VALUES('$fname', '$uname', '$password', '$uemail')");
+            return $reg;
+        }
+
+    }
+    
+
+?>
---
+++ insert.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+
+    include_once('functions.php');
+
+    $insertdata = new DB_con();
+
+    if (isset($_POST['insert'])) {
+        $status = $_POST['status_cm'];
+        $location = $_POST['location_cm'];
+        
+        
+        $sql = $insertdata->insert($status, $location);
+
+        if ($sql) {
+            echo "<script>alert('Record Inserted Successfully!');</script>";
+            echo "<script>window.location.href='index.php'</script>";
+        } else {
+            echo "<script>alert('Something went wrong! Please try again!');</script>";
+            echo "<script>window.location.href='index.php'</script>";
+        }
+    }
+
+?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css"
+        integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mitr&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <style>
+    body {
+        font-family: 'Mitr', sans-serif;
+    }
+
+    .sidebar {
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 100;
+        padding: 90px 0 0;
+        box-shadow: inset -1px 0 0 rgba(0, 0, 0, .1);
+        z-index: 99;
+    }
+
+    @media (max-width: 767.98px) {
+        .sidebar {
+            top: 11.5rem;
+            padding: 0;
+        }
+    }
+
+    .navbar {
+        box-shadow: inset 0 -1px 0 rgba(0, 0, 0, .1);
+    }
+
+    @media (min-width: 767.98px) {
+        .navbar {
+            top: 0;
+            position: sticky;
+            z-index: 999;
+        }
+    }
+
+    .sidebar .nav-link {
+        color: #333;
+    }
+
+    .sidebar .nav-link.active {
+        color: #0d6efd;
+    }
+    </style>
+</head>
+
+<body>
+    <nav class="navbar navbar-light bg-light p-3">
+        <div class="d-flex col-12 col-md-3 col-lg-2 mb-2 mb-lg-0 flex-wrap flex-md-nowrap justify-content-between">
+            <a class="navbar-brand" href="#">
+                <img src="images/cctv_online.png" style="height:40px;weigh:40px;">SMOKE DETECTION
+            </a>
+            <button class="navbar-toggler d-md-none collapsed mb-3" type="button" data-toggle="collapse"
+                data-target="#sidebar" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        <div class="col-12 col-md-5 col-lg-8 d-flex align-items-center justify-content-md-end mt-3 mt-md-0">
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
+                    data-toggle="dropdown" aria-expanded="false">
+                    Hello, คนทำเจค
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <li><a class="dropdown-item" href="#">Settings</a></li>
+                    <li><a class="dropdown-item" href="#">Messages</a></li>
+                    <li><a class="dropdown-item" href="#">Sign out</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container-fluid">
+        <div class="row">
+            <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+                <div class="position-sticky">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">
+                                <i class="fa-solid fa-house-chimney"></i>
+                                <span class="ml-2">Insert</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <i class="fa-solid fa-gear"></i>
+                                <span class="ml-2">Setting</span>
+                            </a>
+                        </li>
+
+                    </ul>
+                </div>
+            </nav>
+            <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4 py-4">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Insert</li>
+                    </ol>
+                </nav>
+                <h1 class="h2">Dashboard</h1>
+                <div class="container">
+                    <a href="index.php" class="btn btn-primary mt-3"><i class="fa-solid fa-chevron-left"></i></a>
+                    <hr>
+                    <form action="" method="post">
+
+                        <div class="mb-3">
+                <label for="location_cm">Location</label>
+                <textarea name="location_cm"cols="30" rows="10" class="form-control" required></textarea>
+            </div>
+
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="status_cm" required>
+                            <label class="form-check-label" for="status_cm">
+                                NO
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="status_cm" required>
+                            <label class="form-check-label" for="status_cm">
+                                OFF
+                            </label>
+                        </div>
+
+                        <button type="submit" name="insert" class="btn btn-success">Insert</button>
+                    </form>
+                </div>
+
+
+            </main>
+
+        </div>
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"
+        integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.js"></script>
+
+
+
+</body>
+
+</html>
